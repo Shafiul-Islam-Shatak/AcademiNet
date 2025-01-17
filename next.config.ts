6906a851dc7fd047c02db22fdd204f98390a3593
jahid <info.jahidulislamjahid@gmail.com>
2025-01-18 01:12:33 +0600

    Static SignIn and SignUp UI design

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ["i.postimg.cc", "i.ibb.co", "itbook.store", "lh3.googleusercontent.com", "drive.google.com", "images.unsplash.com"]
+  },
 };
 
 export default nextConfig;
