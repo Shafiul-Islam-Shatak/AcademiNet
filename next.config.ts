ec78cd4e54823117f9d311905fbb4b05aa35c7b8
Shatak-the-imposter <helloshatak@gmail.com>
2025-01-18 00:35:51 +0600

    navbar and footer added

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ["i.postimg.cc", "i.ibb.co", "lh3.googleusercontent.com"],
+  },
 };
 
 export default nextConfig;
